314c3750784170270c8e18f84b9aa7eaa8abbd57
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-02-15 10:46:38 -0800

    Add global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,8 +8,44 @@ function App(){
     <div>
     <style global jsx>{`
       body {
-        margin: 0;
-        font-family: 'Helvetica Neue';
+        background-image: url('./assets/images/background.jpg');
+        background-repeat: repeat;
+        font-family: 'Roboto', sans-serif;
+        color: #434343;
+      }
+
+      h1 {
+        font-weight: 100;
+        color: #434343;
+        text-align: center;
+        font-size: 65px;
+      }
+
+      h2 {
+        color: #7cb7de;
+        font-weight: 100;
+        text-transform: uppercase;
+        text-align: center;
+        margin-bottom: 30px;
+        font-size: 30px;
+        margin-top: 20px;
+      }
+
+      h3 {
+        color: #7cb7de;
+        font-weight: 300;
+        margin-bottom: 20px;
+        font-size: 24px;
+      }
+
+      h4 {
+        margin-top: 30px;
+        margin-bottom: 6px;
+        font-size: 18px;
+      }
+
+      a {
+        color: #337ab7;
       }
     `} </style>
       <Header/>
