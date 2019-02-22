2144ebcacb3ac225955cde87ac3792e09927447a
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-02-21 21:28:41 -0800

    Break out menu element into separate component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
+import styles from './assets/styles/Menu.css'
+
+function Menu() {
+  return (
+    <div className='menu'>
+      <div><Link to='/#about'><button className='menu-item'>About Me</button></Link></div>
+      <div><Link to='/#work'><button className='menu-item'>My Work</button></Link></div>
+      <div><Link to='/#background'><button className='menu-item'>Background</button></Link></div>
+      <div><Link to='/#interests'><button className='menu-item'>Interests</button></Link></div>
+      <div><Link to='/#contact'><button className='menu-item'>Contact</button></Link></div>
+      <p onClick={this.toggleMenu}>Close</p>
+    </div>
+  )  
+}
+
+export default Menu;
