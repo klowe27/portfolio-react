a59c8dce31f7a2ea0009bf852fb8a0aae0f912ca
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2019-02-22 10:58:25 -0800

    Update bio and project descriptions

--- src/components/assets/content.js
+++ src/components/assets/content.js
@@ -3,7 +3,7 @@ export const Content = {
   phone: '360-910-2110',
   linkedin: 'https://www.linkedin.com/in/kristin-brewer-lowe/',
   github: 'https://github.com/klowe27',
-  bio: 'I\'m a driven and curious marketer turned web developer with a passion for quality, scalable code that solves interesting problems. After earning an MA in English lit, I spent 6+ years using my writing and design skills in marketing. After years of toying with the idea of coding, I quit my job as a product marketing manager to see if programming was right for me. Coding clicked. Now, I\'m leveraging my critical thinking and problem solving skills to build cool things. On weekends, you can find me sipping cider in the backyard, painting, or YouTubing things like \'How to install a tile floor.\' And, you know, doing all those life things like vacuuming and reconciling the budget.',
+  bio: 'I\'m a driven and curious marketer turned web developer with a passion for quality, scalable code that solves interesting problems. After earning an MA in English lit, I spent 6+ years using my writing and design skills in marketing. After years of toying with the idea of coding, I quit my job as a product marketing manager to see if programming was right for me. Coding clicked. Now, I\'m leveraging my critical thinking and problem solving skills to build cool things. On weekends, you can find me sipping cider in the backyard or YouTubing things like \'How to install a tile floor.\' And, you know, doing all those life things like vacuuming and reconciling the budget.',
   background: 'My dad worked for HP for his entire career as a software engineer helping design printers. When I was younger, he used to say I had a great engineering mind based on how I approached projects and found creative solutions. My freshman year of college, I was torn between engineering and English, and ended up pursuing English, which led to a career in marketing. But marketing has never felt like a natural fit for my skills, and I\'ve always wondered if programming could be right for me. I spent enough years wondering, and now I\'m doing something about it.'
 };
 
@@ -12,14 +12,14 @@ export const ProjectListContent = [
     image: 'food',
     name: 'Food Savor',
     description: 'With a small group, I created a web app to let users search for recipes based on expiring ingredients in order to reduce food waste. I designed the UI, parsed and displayed the API response, and was heavily involved in the site architecture.',
-    technologies: ['Pod Save America', 'Serial', 'The Daily', 'Reply All'],
+    technologies: 'Angular, Javascript, Edamam API, Firebase NoSQL database and authentication',
     link: 'https://foodsavorapp-b49fd.firebaseapp.com/'
   },
   {
     image: 'stonewall',
     name: 'Stonewall Wears',
-    description: 'This is an e-commerce site where users can browse products and add them to their cart. Users can log-in with their Google account to save their cart for later.',
-    technologies: 'Angular, Firebase, Firebase Authentication, Typescript and Git',
+    description: 'This is an e-commerce site where users can browse products and add them to their cart. Users can log-in with their Google account to save their cart for later. This was an independent project completed while at Epicodus.',
+    technologies: 'Angular, Javascript, Firebase, Firebase Authentication, Typescript and Git',
     link: 'https://stonewall-wears.firebaseapp.com/'
   },
   {
@@ -47,7 +47,7 @@ export const InterestListContent = [
   {
     label: 'Topics',
     icon: 'favorite',
-    list: ['Minimalis', 'Design', 'Financial Independence', 'Photography']
+    list: ['Minimalism', 'Design', 'Financial Independence', 'Photography']
   },
   {
     label: 'Dream Travel',
